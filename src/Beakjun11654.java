1d0aedfcbcfad244bbc0c8f39a52a84f8c7b975b
kongminseo <kongminseo2002@naver.com>
2024-08-25 21:49:46 +0900

    백준 문자열연습

---
+++ src/Beakjun11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Beakjun11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      int ch=sc.next().charAt(0);
+
+        System.out.println(ch);
+        sc.close();
+    }
+}
