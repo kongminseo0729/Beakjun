5109d6def17314eedc3c3e631007bc575ec5bb46
kongminseo <kongminseo2002@naver.com>
2024-11-26 15:53:55 +0900

    백준 심화

---
+++ src/Beakjun25083.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Beakjun25083{
+    public static void main(String[] args){
+
+
+        String s="         ,r'\"7\n"+
+                "r`-_   ,'  ,/\n"+
+                " \\. \". L_r'\n"+
+                "   `~\\/\n"+
+                "      |\n"+
+                "      |";
+        System.out.print(s);
+
+    }
+}
